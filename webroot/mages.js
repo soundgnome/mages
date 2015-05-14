d780fd36df57606acd640ada7e3254104f2e93db
Jeffrey Gordon <jgordon.oakland@gmail.com>
2015-05-14 04:06:26 +0000

    fix box problem

--- webroot/mages.js
+++ webroot/mages.js
@@ -2098,7 +2098,6 @@ function drawBox() {
             piece[piece.length-1].drawingBoxEndY=drawingBoxEndY;
             piece[piece.length-1].drawingBoxEndX=(piece[piece.length-1].x+Math.abs(piece[piece.length-1].drawingBoxStartX-piece[piece.length-1].drawingBoxEndX)) , 
             piece[piece.length-1].drawingBoxEndY=piece[piece.length-1].y+Math.abs(piece[piece.length-1].drawingBoxStartY-piece[piece.length-1].drawingBoxEndY)
-       console.log(Math.abs(piece[piece.length-1].drawingBoxStartY-piece[piece.length-1].drawingBoxEndY))
         }   
     }
 }
